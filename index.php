124593a68a7e75fcf452fc3e478aa1f26ecafa7b
krisna <krisna.zero.adw@gmail.com>
2025-07-25 17:18:04 +0700

    test

--- index.php
+++ index.php
@@ -1,6 +1,10 @@
 <?php 
 
-include('bitbucket-webhook.php');
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+// include('bitbucket-webhook.php');
 
 
 echo "success";
